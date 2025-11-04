1c5c35a75ac4743bbf75f640fc619e2b6aa16e70
Adar <adar.sarusi@gmail.com>
2025-11-04 23:34:40 +0200

    Changed increment to a random amount(20-60)

--- js/main.js
+++ js/main.js
@@ -4,7 +4,7 @@ var ballSize = 100
 
 function onBallClick() {
     var elBall = document.querySelector('.ball')
-    ballSize += 50
+    ballSize += getRandomInt(20, 60)
     elBall.style.width = ballSize + 'px'
     elBall.style.height = ballSize + 'px'
     elBall.innerHTML = ballSize
