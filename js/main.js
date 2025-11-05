eb45a13baab3453eef0db6dc6f6e7c3b4eb9f241
Adar <adar.sarusi@gmail.com>
2025-11-05 02:47:51 +0200

    Added support to save pervious state and a random bg color ball

--- js/main.js
+++ js/main.js
@@ -1,12 +1,12 @@
 'use strict'
 
-var ballColor = ''
-
-var gBalls = [{ id: 'ball1', size: 100 }, { id: 'ball2', size: 100 }]
+var gBalls = [{ id: 'ball1', size: 100 , prevSize: 100}, { id: 'ball2', size: 100 , prevSize: 100}]
 
 function onBallClick(elBall, maxDiameter) {
 
     const ball = gBalls.find(ball => ball.id === elBall.id)
+
+    ball.prevSize = ball.size
 
     ball.size += getRandomInt(20, 60)
 
@@ -17,23 +17,39 @@ function onBallClick(elBall, maxDiameter) {
     elBall.style.width = ball.size + "px";
     elBall.style.height = ball.size + "px";
     elBall.innerHTML = ball.size
-    
+
+    var randColor = getRandomColor()
+    ball.color = randColor
+    elBall.style.backgroundColor = randColor
+
 }
 
 function onBallClickSwap() {
     var elFirstBall = document.querySelector('.ball1')
     var elSecondBall = document.querySelector('.ball2')
 
-    var heldColor = elSecondBall.style.backgroundColor
-    var heldSize = elSecondBall.style.width
+    const ball1 = gBalls.find(ball => ball.id === elFirstBall.id)
+    const ball2 = gBalls.find(ball => ball.id === elSecondBall.id)
 
-    elSecondBall.style.backgroundColor = elFirstBall.style.backgroundColor
-    elSecondBall.style.width = elFirstBall.style.width
-    elSecondBall.style.height = elFirstBall.style.width
+    ball1.prevSize = ball1.size
+    ball1.prevColor = ball1.color
+    ball2.prevSize = ball2.size
+    ball2.prevColor = ball2.color
 
-    elFirstBall.style.backgroundColor = heldColor
-    elFirstBall.style.width = heldSize
-    elFirstBall.style.height = heldSize
+    ball1.size = ball2.prevSize
+    ball1.color = ball2.prevColor
+    ball2.size = ball1.prevSize
+    ball2.color = ball1.prevColor
+
+    elSecondBall.style.backgroundColor = ball2.color
+    elSecondBall.style.width = ball2.size + 'px'
+    elSecondBall.style.height = ball2.size + 'px'
+    elSecondBall.innerHTML = ball2.size
+    
+    elFirstBall.style.backgroundColor = ball1.color
+    elFirstBall.style.width = ball1.size + 'px'
+    elFirstBall.style.height = ball1.size + 'px'
+    elFirstBall.innerHTML = ball1.size
 }
 
 function onBallClickReduce() {
@@ -56,4 +72,9 @@ function onBallClickReduce() {
     elSecondBall.style.width = ball2.size + 'px'
     elSecondBall.style.height = ball2.size + 'px'
     elSecondBall.innerHTML = ball2.size
+}
+
+function randomBgColor(){
+    var elBody = document.querySelector('body')
+    elBody.style.backgroundColor = getRandomColor()
 }
